f75e43e28ced73dd7cb0437e0bcf286a10c90f42
korenenyles <korenenyles@gmail.com>
2020-02-24 14:54:12 -0500

    adds self to author

--- nested.py
+++ nested.py
@@ -3,7 +3,7 @@
 """
 Module docstring: One line description of what your program does.
 """
-__author__ = "SEAN BAILEY, CHRIS WILSON, KANO MARVEL "
+__author__ = "KOREN NYLES, SEAN BAILEY, CHRIS WILSON, KANO MARVEL "
 
 import sys
 
